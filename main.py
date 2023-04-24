f622c94231aebd473637879c3766b47343eb5434
satyanshu suman <122309559+Satyanshusuman@users.noreply.github.com>
2023-04-24 15:33:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from flask import *
+from flask import Flask,flash,redirect,render_template,request,url_for
 from flask_sqlalchemy import SQLAlchemy
 import json
 from flask_mail import Mail
 
-with open ('D:\python exersice\FLASK\config.json','r') as c:
+with open ('D:\python exersice\Projects\My Blog\config.json','r') as c:
     params = json.load(c)["params"]
    
 local_server=True
 app = Flask(__name__)
 
-app.config.update (
-                   DEBUG = True,
-                   MAIL_SERVER="smtp.gmail.com",
-                   MAIL_PORT="465",
-                   MAIL_USE_SSL=True,
-                   MAIL_USER_NAME=params["gmail_user"],
-                   MAIL_PASSWORD=params["gmail_pass"]
-                   )
-mail= Mail(app)
+app.secret_key ="f65d0847e02e55345c1b1eca1fbbeb6b6a874ceafa757244ee15ece12801cd9d"
+# app.config.update (
+#                    DEBUG = True,
+#                    MAIL_SERVER="smtp.gmail.com",
+#                    MAIL_PORT="465",
+#                    MAIL_USE_SSL=True,
+#                    MAIL_USER_NAME=params["gmail_user"],
+#                    MAIL_PASSWORD=params["gmail_pass"]
+#                    )
+# mail= Mail(app)
 
 if (local_server):
     app.config['SQLALCHEMY_DATABASE_URI']= params['local_uri']
@@ -27,7 +28,7 @@ else:
 
 db=SQLAlchemy(app)
 
-class Contacts(db.Model):
+class Registration(db.Model):
     sno=db.Column(db.Integer,primary_key = True)
     name=db.Column(db.String(45),nullable=False)
     emailid = db.Column(db.String(45),nullable=False)
@@ -46,21 +47,23 @@ def about():
 def post():
         return render_template("post.html",params= params)
 
-@app.route("/contact",methods = ['GET','POST'])
-def contact():
-        if  request.method =='POST':
+@app.route("/register",methods = ['GET','POST'])
+def register():
+    if  request.method =='POST':
             name = request.form.get('name')
             email= request.form.get('email')
             mobile=request.form.get('mobile')
             message=request.form.get('message')
-            entry = Contacts(name= name,emailid=email,mobno=mobile,message = message)
-            db.session.add (entry)
+            entry = Registration(name= name,emailid=email,mobno=mobile,message = message)
+            db.session.add(entry)
             db.session.commit()
-
-            mail.send_message("new message from "+ name,
-                sender= email,recipients=[params['gmail_user']],body=message +"\n"+ mobile)
-           
-        return render_template("contact.html",params = params)
+            
+            # mail.send_message("new message from "+ name,
+            #     sender= email,recipients=[params['gmail_user']],body=message +"\n"+ mobile)
+            
+            flash("Registration succesful!","success")
+            return redirect(url_for("home"))        
+    return render_template("contact.html",params = params)
 
 if __name__=="__main__":
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0")
